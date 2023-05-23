0e16c1c016b58e7ce7271c8427b3342c00d6521f
min8grace <56316476+min8grace@users.noreply.github.com>
2023-05-22 20:27:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { func } from "prop-types";
+import { useState, useEffect } from "react";
 
-function App() {
+ function App(){
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [dollar, setDollar] = useState('');
+  const [chosenCoin, setChosenCoin] = useState(1);
+  const onChange = (event) => setDollar(event.target.value);
+  const onChangeSelect = (event) => setChosenCoin(event.target.value);
+  console.log(chosenCoin);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => 
+    setCoins(json));    
+    setLoading(false);
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coin! ({coins.length} coins)</h1>
+      <label for="coins">Choose a coin:</label>
+      {loading ? <strong>Loading...</strong> :       
+        <select onChange={onChangeSelect}>
+          {coins.map( (coin, index)  => 
+          <option
+            key = {index}
+            value={coin.quotes.USD.price}
+            id={coin.symbol}
+            symbol = {coin.symbol}
+          >
+            {coin.name} ({coin.symbol}) :  {coin.quotes.USD.price} USD </option>)}
+        </select>
+      }     
+        <br />
+        <lable>How much USD would you like to spend on purchasing the Bitcoin?</lable>
+        <input 
+          onChange={onChange} 
+          value={dollar} 
+          placeholder="Please Write your USD..."
+          type="number"
+        />  
+      {dollar ===0? null:<h3>You could buy {Math.round( dollar / chosenCoin)} coins</h3> }  
     </div>
   );
-}
-
+ }
 export default App;
